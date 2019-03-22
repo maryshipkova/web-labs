403408e866155cc799867b503890a7c1176e31d1
Maria Shipkova <maryshipkova@gmail.com>
2019-03-22 21:12:53 +0300

    lab 3

--- lab3/index.js
+++ lab3/index.js
@@ -1,38 +1,123 @@
-let container = document.querySelector('.container');
-let text = document.querySelector('#text');
+var text, img;
 
-function parseQuote(text){
-    console.log(text);
-}
-function updateContent(){
-    // fetch('https://api.forismatic.com/api/1.0/',{
-    //     method:'POST',
-    //     mode: 'no-cors',
-    //     headers: {
-    //         'Access-Control-Allow-Origin':'*',
-    //         'Content-Type': 'text/html'
-    //     },
-    //     body:{
-    //         method: 'getQuote'
-    //     }
-    // })
-    // .then((res)=>{
-    //     console.log(res);
-    // });
-    fetch("https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&jsonp=parseQuote",{
-        method:'POST',
-        mode: 'no-cors',
-        headers: {
-            'Access-Control-Allow-Origin':'*'
+
+function updateView() {
+    if (text && img && img.loaded) {
+        ctx.drawImage(img, 0, 0);
+        var lines = splitText(text);
+        if (lines > 5) {
+            text = img = null;
+            return;
         }
-    })
-    .then((res)=>{
-        console.log(res);
-    });
+        lines.map((line, i) => {
+            ctx.fillText(line, 10, 150 + 27 * (i + 1), 510);
+        });
+    }
 }
 
-document.createElement('canvas')
-document.addEventListener("DOMContentLoaded", function(){
-    updateContent();
-    // setInterval(updateContent, 1000);
+
+function parseQuote(quote) {
+    text = quote.quoteText;
+    updateView();
+}
+
+function updateContent() {
+    text = null
+    img = null;
+    $.ajax({
+        url: 'https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&jsonp=parseQuote',
+        dataType: 'jsonp'
+    });
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://source.unsplash.com/collection/1127163/550x400');
+    xhr.send();
+    xhr.onload = function (data) {
+        img = new Image();
+        img.crossOrigin = "anonymous";
+        img.src = data.srcElement.responseURL;
+        img.onload = function () {
+            img.loaded = true;
+            updateView();
+        }
+    };
+}
+
+function splitText(text) {
+    var txt = text.split(' ');
+    var currWidth = 0;
+    var result = [''];
+    var iter = 0;
+    txt.map(word => {
+        if (currWidth + ctx.measureText(word).width < 500) {
+            currWidth += ctx.measureText(word).width;
+            result[iter] += word + ' ';
+        } else {
+            ++iter;
+            result[iter] = word + ' ';
+            currWidth = ctx.measureText(word).width;
+        }
+    });
+    return result;
+}
+
+//CSS
+var stylesheet = document.createElement('style');
+stylesheet.innerHTML = 
+`body{
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+#text{
+    display: block;
+    border: 2px solid #72E4CD;
+    text-align: center;
+    font-size: 25px;
+    margin-right: 1rem;
+}
+
+#btn{
+    cursor: pointer;
+    border: 2px solid #72E4CD;
+    padding:  1rem 2rem;
+} 
+
+#btn:-webkit-any-link{
+    color: #000;
+    text-decoration:none;
+}
+`;
+document.body.appendChild(stylesheet);
+
+//HTML
+var canvas = document.createElement('canvas');
+canvas.id = 'text';
+canvas.width = 550;
+canvas.height = 400;
+document.body.appendChild(canvas);
+
+var button = document.createElement('a');
+button.id = 'btn';
+button.innerHTML = 'save';
+document.body.appendChild(button);
+
+
+button.addEventListener('click', function () {
+    var dataURL = canvas.toDataURL('image/png');
+    button.href = dataURL;
+    button.download = dataURL;
 });
+
+ctx = canvas.getContext('2d');
+ctx.font = "26px serif";
+
+updateContent()
+setInterval(function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    updateContent();
+}, 5000)
